16e52bc0404ebaaf294842189e6845a30f1b1335
JessicaSung <jessica.sung@utexas.edu>
2016-11-17 21:14:45 -0600

    Added password-hash dependency. Created database schema and data.

---
+++ database/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO gamecard (card_name, item, category) VALUES ('Food', '[The Highball, Mueller Food Trucks, Ego’s, Hipster Eating a Taco, Peddle bar, Bar with a food truck in it, Amy’s Ice Cream, Whole Foods Lamar]', 'Food');
+
+INSERT INTO gamecard (card_name, item, category) VALUES ('Weird', '[Someone fighting with a homeless person, Esther’s Follies, Chicken Shit Bingo, Hippie Hollow, Leslie Cochran statue, Cathedral of Junk, Museum Of The Weird, Guy dancing on a street corner]', 'Weird');
+
+
+INSERT INTO gamecard (card_name, item, category) VALUES ('Landmark', '[The Capital, Graffiti on Castle Hill, Stevie Ray Vaughan Statue, Zilker Park, I Love You So Much mural, Hi How Are You mural, Barton Springs Pool, UT Tower]', 'Landmark');
+
+
+INSERT INTO gamecard (card_name, item, category) VALUES ('Activity', '[Someone paddle boarding, Sixth Street, Congress Ave Bridge At Dusk, Hamilton Pool, Austin Panic Room, Peter Pan Mini-Golf, Someone taking a selfie on Mount Bonnell, Blue Cat Cafe]', 'Activity');
+
+INSERT INTO gamecard (user_name, password, active_card, items_found, badge_level) VALUES ('Bob', 'Smith', 'Food', '[3, 5, 7]', '1');

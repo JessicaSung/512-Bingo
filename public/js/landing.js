df84a0a6545508659ddc1428b27cfbd7008ca357
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-11-21 20:22:46 -0600

    sign in up toggle animation

--- public/js/landing.js
+++ public/js/landing.js
@@ -17,4 +17,29 @@ $(document).ready(function() {
     $(view+' > .landing-form').animate({left: '0px'}, 'slow');
   }
 
+  // --------toggle between sign in/up-------
+
+  $('#toggleSignIn').on('click', function() {
+    toggleSignView('#signUpView', '#signInView');
+  })
+
+  $('#toggleSignUp').on('click', function() {
+    toggleSignView('#signInView', '#signUpView');
+  })
+
+  function toggleSignView(current, next) {
+    $(current).animate({left: "-600px"}, 'slow');
+    $(current).hide('slow');
+    slideToSign(next);
+  }
+
+  // --------landing home------------------
+
+  $('#landing-corner-logo').on('click', function() {
+    $('#signInView').hide();
+    $('#signUpView').hide();
+    $(this).hide();
+    $('#landing').css('right', '0').show();
+  })
+
 })

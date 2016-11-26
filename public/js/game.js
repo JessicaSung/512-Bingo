e5549c494dc514cf2eb257ffb5806780599b7f86
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-11-26 12:00:32 -0600

    tablet modal star show

--- public/js/game.js
+++ public/js/game.js
@@ -3,9 +3,10 @@ $(document).ready(function() {
   // inital AJAX call to database to get marked boxes
 
 
-  // box marking
+  // ------box marking-------------------------
   var box;
 
+  // mobile star
   $('.mobile-box').on('click', function() {
     $('.mobile-modal').slideDown();
     $('body').css('overflow', 'hidden');
@@ -23,5 +24,19 @@ $(document).ready(function() {
     $('.mobile-modal').slideUp();
   })
 
+  // tablet+ star
+  $('.tablet-box').on('click', function() {
+    $('.tablet-modal').slideDown();
+    box = $(this).attr('data-box');
+  })
+
+  $('.tablet-modal .found').on('click', function() {
+    $('.tablet-modal').slideUp();
+    $('[data-box='+box+'] .tablet-star').show();
+  })
+
+  $('.tablet-modal .notFound').on('click', function() {
+    $('.tablet-modal').slideUp();
+  })
 
 })

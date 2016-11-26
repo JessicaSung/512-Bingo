8eaaef9adff43973ab8723fa51e63cc3158c47e5
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-11-26 11:51:20 -0600

    jquery star for mobile

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
 
-  
+  // inital AJAX call to database to get marked boxes
+
+
+  // box marking
+  var box;
+
+  $('.mobile-box').on('click', function() {
+    $('.mobile-modal').slideDown();
+    $('body').css('overflow', 'hidden');
+    box = $(this).attr('data-box');
+  })
+
+  $('.mobile-modal .found').on('click', function() {
+    $('body').css('overflow', 'auto');
+    $('.mobile-modal').slideUp();
+    $('[data-box='+box+'] .star').show();
+  })
+
+  $('.mobile-modal .notFound').on('click', function() {
+    $('body').css('overflow', 'auto');
+    $('.mobile-modal').slideUp();
+  })
 
 
 })

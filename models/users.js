2dae7bb036eb0a05a7855ca049d408e6a7b81e6c
JessicaSung <jessica.sung@utexas.edu>
2016-11-28 15:48:22 -0600

    Added associations for gamecards.js and users.js

--- models/users.js
+++ models/users.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        Users.belongsToMany(models.Gamecards, {through: 'GamecardsUsers'})
       }
     }
   });

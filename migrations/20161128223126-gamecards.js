a0d4dd6debf64ce571d38a02b0b5d327e7d03b8b
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-11-29 12:06:03 -0600

    fixed migration errors

--- migrations/20161128223126-gamecards.js
+++ migrations/20161128223126-gamecards.js
@@ -1,13 +1,31 @@
 'use strict';
 
+var models = require('../models');
+
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return models.Gamecards.bulkCreate(
       [
-        {card_name: "Food", item: "[The Highball, Mueller Food Trucks, Ego’s, Hipster Eating a Taco, Peddle bar, Bar with a food truck in it, Amy’s Ice Cream, Whole Foods Lamar]", category: "Food"},
-        {card_name: "Weird", item: "[Someone fighting with a homeless person, Esther’s Follies, Chicken Shit Bingo, Hippie Hollow, Leslie Cochran statue, Cathedral of Junk, Museum Of The Weird, Guy dancing on a street corner]", category: "Weird"},
-        {card_name: "Landmark", item: "[The Capital, Graffiti on Castle Hill, Stevie Ray Vaughan Statue, Zilker Park, I Love You So Much mural, Hi How Are You mural, Barton Springs Pool, UT Tower]", category: "Landmark"},
-        {card_name: "Activity", item: "[Someone paddle boarding, Sixth Street, Congress Ave Bridge At Dusk, Hamilton Pool, Austin Panic Room, Peter Pan Mini-Golf, Someone taking a selfie on Mount Bonnell, Blue Cat Cafe]", category: "Activity"},
+        {
+          card_name: "Food",
+          item: "[The Highball, Mueller Food Trucks, Ego’s, Hipster Eating a Taco, Peddle bar, Bar with a food truck in it, Amy’s Ice Cream, Whole Foods Lamar]",
+          category: "Food"
+        },
+        {
+          card_name: "Weird",
+          item: "[Someone fighting with a homeless person, Esther’s Follies, Chicken Shit Bingo, Hippie Hollow, Leslie Cochran statue, Cathedral of Junk, Museum Of The Weird, Guy dancing on a street corner]",
+          category: "Weird"
+        },
+        {
+          card_name: "Landmark",
+          item: "[The Capital, Graffiti on Castle Hill, Stevie Ray Vaughan Statue, Zilker Park, I Love You So Much mural, Hi How Are You mural, Barton Springs Pool, UT Tower]",
+          category: "Landmark"
+        },
+        {
+          card_name: "Activity",
+          item: "[Someone paddle boarding, Sixth Street, Congress Ave Bridge At Dusk, Hamilton Pool, Austin Panic Room, Peter Pan Mini-Golf, Someone taking a selfie on Mount Bonnell, Blue Cat Cafe]",
+          category: "Activity"
+        },
       ]
     )
   },
@@ -21,18 +39,6 @@ module.exports = {
         "Landmark",
         "Activity"
         ]},
-        {item: [
-        "[The Highball, Mueller Food Trucks, Ego’s, Hipster Eating a Taco, Peddle bar, Bar with a food truck in it, Amy’s Ice Cream, Whole Foods Lamar]",
-        "[Someone fighting with a homeless person, Esther’s Follies, Chicken Shit Bingo, Hippie Hollow, Leslie Cochran statue, Cathedral of Junk, Museum Of The Weird, Guy dancing on a street corner]",
-        "[The Capital, Graffiti on Castle Hill, Stevie Ray Vaughan Statue, Zilker Park, I Love You So Much mural, Hi How Are You mural, Barton Springs Pool, UT Tower]",
-        "[Someone paddle boarding, Sixth Street, Congress Ave Bridge At Dusk, Hamilton Pool, Austin Panic Room, Peter Pan Mini-Golf, Someone taking a selfie on Mount Bonnell, Blue Cat Cafe]"
-        ]},
-        {category: [
-        "Food",
-        "Weird",
-        "Landmark",
-        "Activity"
-        ]}
       }
     )
   }
--- migrations/20161128223151-users.js
+++ migrations/20161128223151-users.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var models = require('../models');
+
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return models.Users.bulkCreate(
       [
-        {user_name: "Bob", password: "Smith", active_card: "Food", items_found: "[3, 5, 7]", badge_level: "1"}
+        {user_name: "Bob", password: "Smith", active_card: 1, items_found: "[3, 5, 7]", badge_level: 1}
       ]
     )
   },
@@ -12,21 +14,7 @@ module.exports = {
   down: function (queryInterface, Sequelize) {
     return models.Users.destroy(
       {where:
-        {user_name: [
-        "Bob"
-        ]},
-        {password: [
-        "Smith"
-        ]},
-        {active_card: [
-        "Food"
-        ]},
-        {items_found: [
-        "[3, 5, 7]"
-        ]},
-        {badge_level: [
-        "1"
-        ]}
+        {user_name: "Bob"},
       }
     )
   }
